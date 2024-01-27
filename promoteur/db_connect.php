80befef49ffeb666ba751bf4be281d385fcfb8da
latyr16 <latyrnd86@gmail.com>
2024-01-27 13:59:30 +0000

    Mise en ligne premier site operateur

--- promoteur/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$server = "localhost";
-	$username = "bouki";
-	$password = "Test123@";
-	$db = "bd_projet";
-	$conn = mysqli_connect($server, $username, $password, $db);
-?>
--- promoteur/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-        session_start();
-        //$id_agent = $_SESSION['id_agent'];
-        //echo $id_agent;
-        session_destroy();
-        header("Location: ../index.php?id=1");
-?>
